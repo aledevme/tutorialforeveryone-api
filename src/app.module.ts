9c69eda0d7c4c1470ac3602a3b42899ad6465459
Alejandro Gonzalez <Alexgve7sv@outlook.com>
2022-05-10 09:40:15 +0200

    url to cloud

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,8 @@ import { TutorialsController } from './controllers/tutorial.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/tutorialsforeveryone'),
-    //MongooseModule.forRoot('mongodb+srv://aledevme:Alejandro7$.@cluster0.5v234.mongodb.net/tutorialsforeveryone?retryWrites=true&w=majority'),
+    //MongooseModule.forRoot('mongodb://localhost/tutorialsforeveryone'),
+    MongooseModule.forRoot('mongodb+srv://aledevme:c8ldcUVgi9Sh1pZf@cluster0.5v234.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
     MongooseModule.forFeature([{name: Tutorial.name, schema: TutorialSchema}])
   ],
   controllers: [AppController, TutorialsController],
